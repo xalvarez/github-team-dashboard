ce138f4ddfa09a748a6ee6f0b994c818d2ee40db
Xavier Alvarez <xalvarez@users.noreply.github.com>
2019-08-24 19:31:47 +0200

    Add more assertions to test

--- src/test/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubServiceTest.kt
+++ src/test/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubServiceTest.kt
@@ -9,6 +9,7 @@ import io.micronaut.test.annotation.MicronautTest
 import javax.inject.Inject
 import kotlin.test.Test
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 @MicronautTest
 class GitHubServiceTest {
@@ -26,6 +27,18 @@ class GitHubServiceTest {
         val githubDashboardData = gitHubService.fetchDashboardData()
 
         assertNotNull(githubDashboardData.data.organization.team.name)
+        assertTrue { githubDashboardData.data.organization.team.members.nodes.all { it.login.isNotEmpty() } }
+        assertTrue { githubDashboardData.data.organization.team.repositories.nodes.all { it.name.isNotEmpty() } }
+        assertNotNull(githubDashboardData.data.organization.team.repositories.nodes.map { it.pullRequests.nodes })
+        assertTrue {
+            githubDashboardData.data.organization.team.repositories.nodes
+                .filterNot { repository -> repository.pullRequests.nodes.isEmpty() }
+                .all { repository ->
+                    repository.pullRequests.nodes.all {
+                        it.url.isNotEmpty() && it.author.login.isNotEmpty()
+                    }
+                }
+        }
     }
 
     private fun givenSuccessfulGitHubRequest() {
