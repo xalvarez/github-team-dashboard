e424a4c05f0823013e08fd794862e3d5c6463f04
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-02-24 11:53:48 +0100

    Make GitHub Configuration immutable

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
@@ -1,15 +1,13 @@
 package com.github.xalvarez.githubteamdashboard.github
 
 import io.micronaut.context.annotation.ConfigurationProperties
-import javax.inject.Singleton
 
 @ConfigurationProperties(value = GITHUB_PROPERTIES_PREFIX, cliPrefix = [GITHUB_PROPERTIES_PREFIX])
-@Singleton
-class GitHubConfiguration {
-    lateinit var organization: String
-    lateinit var team: String
-    lateinit var token: String
-    lateinit var userAgent: String
+interface GitHubConfiguration {
+    val organization: String
+    val team: String
+    val token: String
+    val userAgent: String
 }
 
 const val GITHUB_PROPERTIES_PREFIX = "github"
