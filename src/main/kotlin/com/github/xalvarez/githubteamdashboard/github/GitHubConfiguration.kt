2b0febdef9e49a588eea5075e8b85523d4c23732
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-04-13 17:42:02 +0200

    Add Validation to Configuration

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
@@ -1,13 +1,14 @@
 package com.github.xalvarez.githubteamdashboard.github
 
 import io.micronaut.context.annotation.ConfigurationProperties
+import javax.validation.constraints.NotEmpty
 
 @ConfigurationProperties(value = GITHUB_PROPERTIES_PREFIX, cliPrefix = [GITHUB_PROPERTIES_PREFIX])
 interface GitHubConfiguration {
-    val organization: String
-    val team: String
-    val token: String
-    val userAgent: String
+    @get:NotEmpty val organization: String
+    @get:NotEmpty val team: String
+    @get:NotEmpty val token: String
+    @get:NotEmpty val userAgent: String
 }
 
 const val GITHUB_PROPERTIES_PREFIX = "github"
