5d91885d0cabd72c56f2be2206bae5857db84748
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-04-14 09:33:36 +0200

    Remove unused reactive call

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubClient.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubClient.kt
@@ -2,12 +2,11 @@ package com.github.xalvarez.githubteamdashboard.github
 
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
-import io.reactivex.Single
 
 @BearerToken
 @Client("\${github.apiUrl}")
 interface GitHubClient {
 
     @Post
-    fun fetchDashboardData(query: String): Single<GithubDashboardData>
+    fun fetchDashboardData(query: String): GithubDashboardData
 }
