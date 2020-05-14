f7e5163c4dccf92f5de073961519c3683a5fa2c1
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-05-14 17:22:44 +0200

    Update scripts and css

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubConfiguration.kt
@@ -1,9 +1,11 @@
 package com.github.xalvarez.githubteamdashboard.github
 
 import io.micronaut.context.annotation.ConfigurationProperties
+import io.micronaut.context.annotation.Context
 import javax.validation.constraints.NotEmpty
 
 @ConfigurationProperties(value = GITHUB_PROPERTIES_PREFIX, cliPrefix = [GITHUB_PROPERTIES_PREFIX])
+@Context
 interface GitHubConfiguration {
     @get:NotEmpty val organization: String
     @get:NotEmpty val team: String
