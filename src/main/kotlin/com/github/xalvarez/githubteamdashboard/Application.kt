0f5d417261aa7adb717a679f54806391c95c6541
Xavier Alvarez <xalvarez@users.noreply.github.com>
2022-08-27 20:43:20 +0200

    Update micronaut dependencies

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/Application.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/Application.kt
@@ -5,7 +5,6 @@ import io.micronaut.runtime.Micronaut.build
 fun main(vararg args: String) {
     build()
         .args(*args)
-        .packages("com.github.xalvarez.githubteamdashboard")
         .banner(false)
         .start()
 }
