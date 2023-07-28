4c1d2af6ca9b471356fc3d5db59be6b166c72076
Xavier Alvarez <xalvarez@users.noreply.github.com>
2023-07-28 07:37:46 +0200

    Switch from serde to jackson (#431)

--- src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubResponseBody.kt
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/GitHubResponseBody.kt
@@ -1,40 +1,46 @@
 package com.github.xalvarez.githubteamdashboard.github
 
-import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.annotation.JsonFormat.Shape.NUMBER
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.micronaut.serde.annotation.Serdeable
 import java.time.ZonedDateTime
 
+@Serdeable
 data class GithubDashboardData(
     val data: Data
 )
 
+@Serdeable
 data class Data(
     val organization: Organization
 )
 
+@Serdeable
 data class Organization(
     val team: Team
 )
 
+@Serdeable
 data class Team(
     val name: String,
     val members: Members,
     val repositories: Repositories
 )
 
+@Serdeable
 data class Members(
     val nodes: List<MembersNode>
 )
 
+@Serdeable
 data class MembersNode(
     val login: String
 )
 
+@Serdeable
 data class Repositories(
     val nodes: List<Repository>
 )
-
+@Serdeable
 data class Repository(
     val name: String,
     val pullRequests: PullRequests,
@@ -44,16 +50,19 @@ data class Repository(
     val alertsUrl: String by lazy { url +  ALERTS_URL_SUFFIX }
 }
 
+@Serdeable
 data class VulnerabilityAlerts(
     @JsonProperty("totalCount")
-    @JsonFormat(shape = NUMBER)
+    @Serdeable.Deserializable(using = TotalCountSerde::class)
     val arePresent: Boolean
 )
 
+@Serdeable
 data class PullRequests(
     val nodes: List<PullRequestNode>
 )
 
+@Serdeable
 data class PullRequestNode(
     val url: String,
     val createdAt: ZonedDateTime,
@@ -64,30 +73,36 @@ data class PullRequestNode(
     val commits: Commits
 )
 
+@Serdeable
 data class Author(
     val login: String
 )
 
+@Serdeable
 data class Review(
     val nodes: List<ReviewNode>
 )
 
+@Serdeable
 data class ReviewNode(
     val state: String
 )
 
+@Serdeable
 data class Commits(
      val nodes: List<CommitNode>
 )
 
+@Serdeable
 data class CommitNode(
       val commit: Commit
 )
-
+@Serdeable
 data class Commit(
       val statusCheckRollup: CommitStatusCheckState?
 )
 
+@Serdeable
 data class CommitStatusCheckState(
       val state: String
 )
---
+++ src/main/kotlin/com/github/xalvarez/githubteamdashboard/github/TotalCountSerde.kt
@@ -0,0 +1,27 @@
+package com.github.xalvarez.githubteamdashboard.github
+
+import io.micronaut.core.type.Argument
+import io.micronaut.serde.Decoder
+import io.micronaut.serde.Deserializer
+import io.micronaut.serde.Encoder
+import io.micronaut.serde.Serde
+import io.micronaut.serde.Serializer
+
+class TotalCountSerde: Serde<Boolean> {
+    @Suppress("EmptyFunctionBlock")
+    override fun serialize(
+        encoder: Encoder,
+        context: Serializer.EncoderContext,
+        type: Argument<out Boolean>,
+        value: Boolean
+    ) {
+    }
+
+    override fun deserialize(
+        decoder: Decoder,
+        context: Deserializer.DecoderContext,
+        type: Argument<in Boolean>
+    ): Boolean {
+        return decoder.decodeInt() > 0;
+    }
+}
