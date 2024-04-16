e11f3094ee1da71f6c760f80b8543ca206cd07aa
Xavier Alvarez <xalvarez@users.noreply.github.com>
2024-04-16 20:51:37 +0200

    Add author filter

--- src/main/resources/views/public/dashboard.js
+++ src/main/resources/views/public/dashboard.js
@@ -14,4 +14,36 @@ function setLastUpdateTime () {
   lastUpdateTimeElement.textContent = new Date().toLocaleTimeString()
 }
 
+function filterRows(element) {
+  const author = element.value;
+  const isChecked = element.checked;
+  const isAll = author === 'all';
+  const checkboxes = document.querySelectorAll('.form-check-input');
+  const rows = document.querySelectorAll('tr[data-author]');
+  const allCheckbox = document.querySelector('.form-check-input[value="all"]');
+  const noAuthorsSelectedMessage = document.getElementById('no-authors-selected');
+
+  if (isAll) {
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = isChecked;
+    });
+  } else {
+    if (!isChecked) {
+      allCheckbox.checked = false;
+    } else {
+      allCheckbox.checked = Array.from(checkboxes).every(
+          checkbox => checkbox.checked);
+    }
+  }
+
+  const anyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+  rows.forEach((row) => {
+    const rowAuthor = row.getAttribute('data-author');
+    const isRowChecked = document.querySelector(`.form-check-input[value="${rowAuthor}"]`)?.checked;
+    row.style.display = isRowChecked ? '' : 'none';
+  });
+
+  noAuthorsSelectedMessage.style.display = anyChecked ? 'none' : '';
+}
+
 refreshDashboard()
